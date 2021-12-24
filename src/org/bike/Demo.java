3a09cc2136c5114b953c673fd901dbd454918554
Jebaraj18 <agastinthekjk@gmail.com>
2021-12-24 10:34:12 +0530

    fytgf

---
+++ src/org/bike/Demo.java
@@ -0,0 +1,15 @@
+package org.bike;
+
+public class Demo {
+public static void main(String[] args) {
+	
+	int a,b,temp;
+	a=15;
+	b=20;
+	System.out.println("before swapping : a,b="+a+"," + + b);
+	temp=a;
+	a=b;
+	b=temp;
+	System.out.println("After swapping: a,b="+a+"," + + b);
+}
+}
